4f25c0a1dbb820dc35423473cc6a2d068663907e
Omar Oporto Bernal <omar.oportober@gmail.com>
2023-11-25 11:09:06 -0400

    Completed all the exercises from unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,44 +7,80 @@ const Button = ({ handleClick, label }) => {
     </button>
   )
 }
+//------------------------------------------------------- 1st Version 1.8  ----------------------------------------------------
+// const All = ({ good, neutral, bad }) => (
+//   <>
+//     all {bad + neutral + good}<br/>
+//   </>
+// )
+// const Average = ({ good, neutral, bad }) => {
 
-const All = ({ good, neutral, bad }) => (
+//   const average = (good !==0 || neutral !==0 || bad !== 0 ) ? (good - bad)/(bad+neutral+good) : 0
+
+//   return(
+//     <>
+//       average {average}<br/>
+//     </>
+//   )
+// }
+
+// const Positive = ({ good, neutral, bad }) => {
+
+//   const positive = (good !==0 || neutral !==0 || bad !== 0 ) ? (good)/(bad+neutral+good) : 0
+
+//   return(
+//     <>
+//       positive {positive}<br/>
+//     </>
+//   )
+// }
+
+// const Info = ({good, neutral, bad}) => (
+//   <p>
+//     good {good}<br/>
+//     neutral {neutral}<br/>
+//     bad {bad}<br/>
+//     <All bad={bad} neutral={neutral} good={good}/>
+//     <Average bad={bad} neutral={neutral} good={good}/>
+//     <Positive bad={bad} neutral={neutral} good={good}/>
+//   </p>
+// )
+
+const StaticsLine = ({text, value}) => (
   <>
-    all {bad + neutral + good}<br/>
+    <td> {text} </td>
+    <td> {value} </td>
   </>
 )
-const Average = ({ good, neutral, bad }) => {
 
-  const average = (good !==0 || neutral !==0 || bad !== 0 ) ? (good - bad)/(bad+neutral+good) : 0
+const Statics = ({good, neutral, bad}) => {
 
-  return(
-    <>
-      average {average}<br/>
-    </>
-  )
+  const all = bad + neutral + good
+  const average = ( good !==0 || neutral !==0 || bad !== 0 ) ? (good - bad)/(bad+neutral+good) : 0
+  const positive = ( good !==0 || neutral !==0 || bad !== 0 ) ? (good)/(bad+neutral+good) : 0
+
+  if(good !== 0 || neutral !== 0 || bad !== 0) {
+    return(
+      <table>
+        <tbody>
+          <tr><StaticsLine text="good" value={good}/></tr>
+          <tr><StaticsLine text="neutral" value={neutral}/></tr>
+          <tr><StaticsLine text="bad" value={bad}/><tr/></tr>
+          <tr><StaticsLine text="all" value={all}/><tr/></tr>
+          <tr><StaticsLine text="average" value={average}/></tr>
+          <tr><StaticsLine text="positive" value={positive}/></tr>
+        </tbody>
+      </table>
+    )
+  }
+  else {
+    return(
+      <div>
+        No feedback given
+      </div>
+    )
+  }
 }
-
-const Positive = ({ good, neutral, bad }) => {
-
-  const positive = (good !==0 || neutral !==0 || bad !== 0 ) ? (good)/(bad+neutral+good) : 0
-
-  return(
-    <>
-      positive {positive}<br/>
-    </>
-  )
-}
-
-const Info = ({good, neutral, bad}) => (
-  <p>
-    good {good}<br/>
-    neutral {neutral}<br/>
-    bad {bad}<br/>
-    <All bad={bad} neutral={neutral} good={good}/>
-    <Average bad={bad} neutral={neutral} good={good}/>
-    <Positive bad={bad} neutral={neutral} good={good}/>
-  </p>
-)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -61,7 +97,7 @@ const App = () => {
       <Button handleClick={() => increaseByOne(setNeutral)} label="neutral" />
       <Button handleClick={() => increaseByOne(setBad)} label="bad" />
       <h1>statics</h1>
-      <Info good={good} neutral={neutral} bad={bad}/>
+      <Statics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
